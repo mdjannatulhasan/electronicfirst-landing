04905c5118e1eae11ce809ce6991137228052f54
Shah Muhammad Jannatul Hasan <shahmdjhm@gmail.com>
2023-08-20 02:42:07 +0600

    Form and responsive Menu added

--- js/main.js
+++ js/main.js
@@ -51,4 +51,47 @@ document.addEventListener("DOMContentLoaded", function () {
 			}
 		});
 	});
+
+	const mobileMenu = document.querySelector(".mobile-menu");
+	const hamburger = document.querySelector(".hamburger");
+
+	hamburger.addEventListener("click", function () {
+		mobileMenu.classList.toggle("open");
+	});
+
+	/**
+	 * Search Clicked
+	 */
+	const searchIcon = document.querySelector(".search-icon");
+	const searchContainer = document.querySelector(".search");
+	const closeButton = document.querySelector(".close");
+
+	searchIcon.addEventListener("click", function () {
+		searchContainer.classList.add("expanded");
+		searchIcon.style.display = "none";
+		closeButton.classList.add("opened");
+
+		searchContainer.addEventListener("transitionend", function () {
+			searchContainer.querySelector("form").style.display = "flex !important";
+			closeButton.style.display = "flex !important";
+			searchIcon.style.display = "none";
+
+			searchContainer.removeEventListener("transitionend", this);
+		});
+	});
+
+	closeButton.addEventListener("click", function () {
+		searchContainer.classList.remove("expanded");
+		searchContainer.querySelector("form").style.display = "none !important";
+		closeButton.classList.remove("opened");
+
+		searchContainer.addEventListener("transitionend", function () {
+			closeButton.style.display = "none !important";
+			searchContainer.querySelector("form").style.display = "none !important";
+
+			searchIcon.style.display = "block";
+
+			searchContainer.removeEventListener("transitionend", this);
+		});
+	});
 });
