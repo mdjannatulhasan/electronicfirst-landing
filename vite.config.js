4e159b99999a07c159e4a00f108a4f3edf462c6b
Shah Muhammad Jannatul Hasan <shahmdjhm@gmail.com>
2023-08-20 19:38:02 +0600

    Build in doc

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,8 @@ export default {
 			},
 		},
 	},
+	base: "electronicfirst-landing", // Replace with your GitHub repository name
+	build: {
+		outDir: "docs", // Change the output directory to "docs" for GitHub Pages
+	},
 };
